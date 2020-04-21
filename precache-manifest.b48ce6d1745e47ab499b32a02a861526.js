0dbc1dfb1745436b6b31954aaf09da6c7f814729
Siddharamesh <siddharameshkm@gmail.com>
2020-04-21 23:24:39 +0530

    Updates

---
+++ precache-manifest.b48ce6d1745e47ab499b32a02a861526.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a301e02ec986c74145448f7debcb969",
+    "url": "/covid19site/index.html"
+  },
+  {
+    "revision": "87ead59bca2311c6f64a",
+    "url": "/covid19site/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "dc96dc9414db1212d954",
+    "url": "/covid19site/static/css/main.e43acdf7.chunk.css"
+  },
+  {
+    "revision": "87ead59bca2311c6f64a",
+    "url": "/covid19site/static/js/2.5e36f4c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19site/static/js/2.5e36f4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc96dc9414db1212d954",
+    "url": "/covid19site/static/js/main.6677df1a.chunk.js"
+  },
+  {
+    "revision": "6b6ec9bbeb55bcbf44a8",
+    "url": "/covid19site/static/js/runtime-main.33ad8a9a.js"
+  },
+  {
+    "revision": "28195c14e6a271f42ca0994fac4fb0e4",
+    "url": "/covid19site/static/media/AbrilFatface-Regular.28195c14.ttf"
+  }
+]);
